f56197762dd8105cdb6a51433c2abd18272018ac
raghava bhairwade <raghavabhairwade@raghavas-MacBook-Pro.local>
2020-06-30 19:30:45 +0100

    "porduct remove and update"

--- routes/product.js
+++ routes/product.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
 const { userById } = require("..//controllers/user");
-const { productById ,read, create, remove} = require('../controllers/product');
+const { productById ,read, create, remove}, update = require('../controllers/product');
 const { Router } = require('express');
 
 // router
 router.post('/product/create/:userId', requireSignin, isAdmin, isAuth, create);
 router.get('/product/:productId', read);
 router.delete('/product/:productId/:userId', requireSignin, isAdmin, isAuth, remove)
+router.put('/product/:productId/:userId', requireSignin, isAdmin, isAuth, update)
+
 router.param("userId", userById)
 router.param("productId", productById)
 
