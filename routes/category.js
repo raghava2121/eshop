5d10061c7fd74f8842235cfe4b45908c475a177b
raghava bhairwade <raghavabhairwade@raghavas-MacBook-Pro.local>
2020-07-01 20:10:32 +0100

    category update remove list

--- routes/category.js
+++ routes/category.js
@@ -3,11 +3,21 @@ const router = express.Router();
 const { userSignupValidator } = require("../validator/index")
 const { create } = require('../controllers/category');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
-const { userById } = require("..//controllers/user")
+const { userById } = require("..//controllers/user");
+const { route } = require('./auth');
+const { categoryById, read, update, remove, list} = require("..//controllers/category");
 
 // router
 router.post('/category/create/:userId', requireSignin, isAdmin, isAuth, create);
 
-router.param("userId", userById)
+router.get('/category/:categoryId',read);
+router.put('/category/:categoryId/:userId', requireSignin, isAdmin, isAuth, update);
+router.delete('/category/:categoryId/:userId', requireSignin, isAdmin, isAuth, remove);
+
+router.get('/categories',list);
+
+router.param('categoryId', categoryById)
+router.param("userId", userById);
+
 
 module.exports = router;
