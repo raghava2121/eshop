43af3d0d9995cb53e786b95df059f7f6dc861e4e
raghava bhairwade <raghavabhairwade@raghavas-MacBook-Pro.local>
2020-07-02 00:05:54 +0100

    product by realted route

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
 const { userById } = require("..//controllers/user");
-const { productById, read, create, remove, update, list } = require('../controllers/product');
+const { productById, read, create, remove, update, list, listRelated } = require('../controllers/product');
 const { Router } = require('express');
 
 // router
@@ -10,8 +10,8 @@ router.post('/product/create/:userId', requireSignin, isAdmin, isAuth, create);
 router.get('/product/:productId', read);
 router.delete('/product/:productId/:userId', requireSignin, isAdmin, isAuth, remove)
 router.put('/product/:productId/:userId', requireSignin, isAdmin, isAuth, update)
-
 router.get("/products", list)
+router.get("/products/related/:productId", listRelated)
 router.param("userId", userById)
 router.param("productId", productById)
 
