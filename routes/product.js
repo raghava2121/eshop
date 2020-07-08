88c1344d3ffd9d375f883e8048271462c6f9445f
raghava bhairwade <raghavabhairwade@raghavas-MacBook-Pro.local>
2020-07-08 20:17:27 +0100

    photo by product id

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
 const { userById } = require("..//controllers/user");
-const { productById, read, create, remove, update, list, listRelated, listCategories} = require('../controllers/product');
+const { productById, read, create, remove, update, list, listRelated, listCategories, listBySearch,photo } = require('../controllers/product');
 const { Router } = require('express');
 
 // router
@@ -11,10 +11,14 @@ router.get('/product/:productId', read);
 router.delete('/product/:productId/:userId', requireSignin, isAdmin, isAuth, remove)
 router.put('/product/:productId/:userId', requireSignin, isAdmin, isAuth, update)
 router.get("/products", list)
-router.get("/products/related/:productId", listRelated)
+router.get("/products/related/:productId", listRelated);
 router.param("userId", userById)
 router.param("productId", productById);
 
-router.get("/products/categories", listCategories)
+// route - make sure its post
+router.post("/products/by/search", listBySearch);
 
+router.get("/products/categories", listCategories);
+router.get("/products/photo/:productId", photo);
 module.exports = router;
+
